807bce5442ab0fecac19f35322d6ac9f7a6e2dba
GraceGenuardi <graziagenuardi96@gmail.com>
2023-04-05 17:57:45 +0200

    EDIT+FEAT stampato array

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,10 +2,28 @@
 
 @section('content')
 
+<div>
+    <div class="container">
+        
+    </div>
+    <div class="container">
+        <div class="row">
+            <!-- {{-- @dump($comics) --}} -->
+            @foreach ($comic as $comic)
+            
+                <div class="col-4">
+                    <!-- <img src="{{ $comic['thumb'] }}" alt="">{{ $comic['thumb'] }} -->
+                    <h3>{{ $comic['title'] }}</h3>
+                    <p>{{ $comic['description'] }}</p>
+                    <span>{{ $comic['price'] }}</span>
 
-<div class="container">
-    <h2>contenuto arrayyy</h2>
+                </div>
+                
+            @endforeach
+        </div>
+    </div>
 </div>
 
-
 @endsection
+
+@section('page-title',$page_title)
