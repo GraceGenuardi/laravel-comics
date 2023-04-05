40cdd9a760c6a372d4e671756246e2968e440b2c
GraceGenuardi <graziagenuardi96@gmail.com>
2023-04-05 18:29:07 +0200

    EDIT+STYLE stile dato a header e footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,5 +1,17 @@
-<footer>
+<footer class="main-footer">
 <div class="container">
-    <h1>FOOTER</h1>
+    
+   <ul class="d-flex list-unstyled gap-5 text-light">
+
+     <li>Hame</li>
+     <li>Videos</li>
+     <li>Films</li>
+     <li>HD</li>
+     <li>Marvel</li>
+     <li>DC</li>
+     <li> <button type="button" class="btn btn-secondary">Iscriviti</button></li>
+     <li> <button type="button" class="btn btn-secondary">Scopri di più!</button></li>
+
+  </ul>
 </div>
 </footer>
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,5 +1,15 @@
 <header class="main-header">
   <div class="container">
-    <h1>HEADER</h1>
-  </div>
+    <ul class="d-flex list-unstyled gap-5 text-primary">
+
+      <li>Hame</li>
+      <li>Videos</li>
+      <li>Films</li>
+      <li>HD</li>
+      <li>Marvel</li>
+      <li>DC</li>
+      <li> <button type="button" class="btn btn-primary btn-lg">Iscriviti</button></li>
+      <li> <button type="button" class="btn btn-primary btn-lg">Scopri di più!</button></li>
+      
+    </ul>
 </header>
